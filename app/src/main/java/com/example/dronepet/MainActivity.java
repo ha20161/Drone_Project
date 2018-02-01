6322ec88dd15668b538ef27e5066a4ffb7748853
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2018-02-01 17:51:12 +0000

    parrot sdk compiled

--- app/src/main/java/com/example/dronepet/MainActivity.java
+++ app/src/main/java/com/example/dronepet/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.dronepet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parrot.arsdk.ARSDK;
+
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        ARSDK.loadSDKLibs();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
